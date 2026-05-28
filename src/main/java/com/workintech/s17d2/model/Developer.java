9fa7ad08e9f39e2e08431ecc1ed848e25e605900
Anıl Sevinç <contact@anilsevinc.net>
2026-05-28 22:26:24 +0300

    feat: add Developer model and Experience enum

---
+++ src/main/java/com/workintech/s17d2/model/Developer.java
@@ -0,0 +1,49 @@
+package com.workintech.model;
+
+public class Developer {
+
+    private Integer id;
+    private String name;
+    private Double salary;
+    private Experience experience;
+
+    public Developer(Integer id, String name, Double salary, Experience experience) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.experience = experience;
+    }
+
+    // getters & setters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public Experience getExperience() {
+        return experience;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void setExperience(Experience experience) {
+        this.experience = experience;
+    }
+}
