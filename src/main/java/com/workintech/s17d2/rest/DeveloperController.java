d978587d299f0e0a3a0eae796ce0fc629c5791a5
Anıl Sevinç <contact@anilsevinc.net>
2026-05-29 15:18:18 +0300

    feat: create DeveloperController with dependency injection and map initialization

---
+++ src/main/java/com/workintech/s17d2/rest/DeveloperController.java
@@ -0,0 +1,30 @@
+package com.workintech.s17d2.rest;
+
+import com.workintech.s17d2.model.Developer;
+import com.workintech.s17d2.tax.Taxable;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/developers")
+public class DeveloperController {
+
+    private final Taxable taxable;
+
+    private Map<Integer, Developer> developers;
+
+    @Autowired
+    public DeveloperController(Taxable taxable) {
+        this.taxable = taxable;
+    }
+
+    @PostConstruct
+    public void init() {
+        developers = new HashMap<>();
+    }
+}
