a25141ea74d03e00251f57d7be399084d2f04c1a
Anıl Sevinç <contact@anilsevinc.net>
2026-05-29 15:22:42 +0300

    feat: add developer get endpoints

--- src/main/java/com/workintech/s17d2/rest/DeveloperController.java
+++ src/main/java/com/workintech/s17d2/rest/DeveloperController.java
@@ -4,10 +4,11 @@ import com.workintech.s17d2.model.Developer;
 import com.workintech.s17d2.tax.Taxable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -26,5 +27,15 @@ public class DeveloperController {
     @PostConstruct
     public void init() {
         developers = new HashMap<>();
+    }
+
+    @GetMapping
+    public List<Developer> getAllDevelopers() {
+        return new ArrayList<>(developers.values());
+    }
+
+    @GetMapping("/{id}")
+    public Developer getDeveloperById(@PathVariable Integer id) {
+        return developers.get(id);
     }
 }
