bed50b1f08d7e99f90f5eab952f9568dd6ce4d6f
Anıl Sevinç <contact@anilsevinc.net>
2026-05-29 15:25:19 +0300

    feat: add developer post endpoint with tax calculation

--- src/main/java/com/workintech/s17d2/rest/DeveloperController.java
+++ src/main/java/com/workintech/s17d2/rest/DeveloperController.java
@@ -11,6 +11,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.workintech.s17d2.model.Experience;
+import com.workintech.s17d2.model.JuniorDeveloper;
+import com.workintech.s17d2.model.MidDeveloper;
+import com.workintech.s17d2.model.SeniorDeveloper;
+
 @RestController
 @RequestMapping("/developers")
 public class DeveloperController {
@@ -37,5 +45,55 @@ public class DeveloperController {
     @GetMapping("/{id}")
     public Developer getDeveloperById(@PathVariable Integer id) {
         return developers.get(id);
+    }
+
+    @PostMapping
+    public Developer addDeveloper(@RequestBody Developer developer) {
+
+        Developer newDeveloper;
+
+        if (developer.getExperience() == Experience.JUNIOR) {
+
+            double salary =
+                    developer.getSalary()
+                            - (developer.getSalary() * taxable.getSimpleTaxRate() / 100);
+
+            newDeveloper =
+                    new JuniorDeveloper(
+                            developer.getId(),
+                            developer.getName(),
+                            salary,
+                            developer.getExperience());
+
+        } else if (developer.getExperience() == Experience.MID) {
+
+            double salary =
+                    developer.getSalary()
+                            - (developer.getSalary() * taxable.getMiddleTaxRate() / 100);
+
+            newDeveloper =
+                    new MidDeveloper(
+                            developer.getId(),
+                            developer.getName(),
+                            salary,
+                            developer.getExperience());
+
+        } else {
+
+            double salary =
+                    developer.getSalary()
+                            - (developer.getSalary() * taxable.getUpperTaxRate() / 100);
+
+            newDeveloper =
+                    new SeniorDeveloper(
+                            developer.getId(),
+                            developer.getName(),
+                            salary,
+                            developer.getExperience());
+        }
+
+        developers.put(newDeveloper.getId(), newDeveloper);
+
+        return newDeveloper;
     }
 }
